b2b37ae3e9de7a7509d0d8270bb5b0b8e43e59e2
ehp2021 <emmahpaik@gmail.com>
2021-12-02 10:38:54 -0600

    put

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ app.use(cors())
 app.get("/", (req, res) => {
     res.send(importData);
 });
+app.put("/:id", (req, res)=>{
+    const id = req.params.id;
+    importData
+        .findOne({id: id})
+        .update({minted: true},
+            function(err){
+                if (!err){
+                  res.send("Successfully updated to true.");
+                } else {
+                  res.send(err);
+                }
+              })
+})
 
 app.listen(port, () => {
     console.log(`listening on port http://localhost:${port}`)
